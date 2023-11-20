732f8c6f3b4062680e88e5431a1eb634d83cb5af
fstahl17 <93704519+fstahl17@users.noreply.github.com>
2023-11-20 09:01:47 +0100

    openraregold

--- ôpenRareGoldPack/run.py
+++ ôpenRareGoldPack/run.py
@@ -1,13 +1,11 @@
 import pyautogui
 import time
-import getpixelcolor as getpixelcolor
 
-while (True):
-    pyautogui.moveTo(1909, 2014)
-    pyautogui.click(1909, 2014)
-    time.sleep(3)
-    pyautogui.click(1909, 2014)
-    time.sleep(1)
+pyautogui.mouseDown()
+time.sleep(5)
+pyautogui.mouseUp()
+
+while True:
     pyautogui.moveTo(1908, 1677)
     pyautogui.click(1908, 1677)
     time.sleep(4)
@@ -16,3 +14,5 @@ while (True):
     pyautogui.moveTo(1909, 2014)
     pyautogui.click(1909, 2014)
     time.sleep(1)
+
+    #du oarsch
