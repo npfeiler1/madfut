41c150f37f7396337068211d3abab71d55609c40
fstahl17 <93704519+fstahl17@users.noreply.github.com>
2023-11-20 11:23:04 +0100

    Update run.py

--- Daten/run.py
+++ Daten/run.py
@@ -5,7 +5,7 @@ def addPlayer(overall_stat,position, name, pac, sho, pas, dri, def_value, phy, c
     conn = sqlite3.connect('spielerdaten.db')
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO Fussballspieler (overall_stat, position, name, pac, sho, pas, dri, def, phy, country, card_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        (overall_stat, position, name, pac, sho, pas, dri, def_value, phy, country, card_type))
+        "INSERT INTO Fussballspieler (overall_stat, position, name, pac, sho, pas, dri, def, phy, country, liga, club, card_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        (overall_stat, position, name, pac, sho, pas, dri, def_value, phy, country, liga, club, card_type))
     conn.commit()
     conn.close()
